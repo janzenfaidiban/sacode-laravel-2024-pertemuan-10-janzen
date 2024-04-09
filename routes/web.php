39c16c81ee987fd4d30f94ec75720ed4efa86852
username_anjastabuni <reblytabuni2004@gmail.com>
2024-04-09 18:54:56 +0700

    feat: ubah file welcome mejadi rebly.blande.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Welcome';
+});
+Route::get('/rebly', function () {
+    return view('rebly');
 });
