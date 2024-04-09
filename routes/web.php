b03cd39550506da34e103db05d68cd938f5816df
melpenyogi <melpenyogi2019@gmail.com>
2024-04-09 20:54:56 +0900

    feat:ubah file welcome menjadi melpen.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('welcome');
+});
+
+Route::get('/melpen', function () {
+    return view('melpen');
 });
