b1377aa2965e30b60e3a14150513c8ca931cf33b
maikelkayame <kayamebate@gmail.com>
2024-04-09 20:54:55 +0900

    feat:ubah file welcome menjadi maikel.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
+});
+
+Route::get('/maikel', function () {
+    return view('maikel');
 });
