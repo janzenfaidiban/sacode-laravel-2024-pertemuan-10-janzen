f5338f0b1a73f61c50d3c46b5f0de94f9e57af61
tenouye-marten <tenouyemarten@gmail.com>
2024-04-09 20:54:57 +0900

    feat: ubah file welcome menjadi marthen.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "welcome";
+});
+
+Route::get('/marthen', function () {
+    return view('marthen');
 });
