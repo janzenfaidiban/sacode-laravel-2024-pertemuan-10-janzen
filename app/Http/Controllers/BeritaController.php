3774b4f1af7426a39a1c79975302106b753dd5e8
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-04-23 19:22:50 +0900

    feat: tambah BeritaController

---
+++ app/Http/Controllers/BeritaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BeritaController extends Controller
+{
+    //
+}
+
+// perintah Git memuat controller kosong
+// php artisan make:controller BeritaController
