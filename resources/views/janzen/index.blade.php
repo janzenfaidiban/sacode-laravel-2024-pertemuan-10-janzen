9306073e50cc0b205d25ac5718cc17020e21e242
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-04-26 18:24:22 +0900

    update janzen

--- resources/views/janzen/index.blade.php
+++ resources/views/janzen/index.blade.php
@@ -3,6 +3,23 @@
 </h1>
 
 {{-- 
+    
+    `git branch dev-janzen`
+
+    `git checkout dev-janzen`
+
+    `git branch`
+
+    `git push origin dev-janzen -f`
+
+    `git merge main`
+
+--}}
+
+
+
+
+{{-- 
     git add . 
 
     git commit -m "tambah folder janzen dan file index"
